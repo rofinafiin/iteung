85c0c47c84f049319353a9620ce7f182084887b9
rofinafiin <rofinafiisr@gmail.com>
2023-03-20 14:14:44 +0700

    add post

--- controller/controller.go
+++ controller/controller.go
@@ -74,10 +74,9 @@ func InsertData(c *fiber.Ctx) error {
 
 func InsertDataComplain(c *fiber.Ctx) error {
 	model := new(hdbackend.JumlahComplain)
-	err := c.BodyParser(model)
+	err := hdbackend.InsertJumlahComplain(config.MongoConn, model.Bulan, model.Tahun, model.Jumlah)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, "Parameter gak lengkap")
 	}
-	insdata := hdbackend.InsertJumlahComplain(config.MongoConn, model.Bulan, model.Tahun, model.Jumlah)
-	return c.JSON(insdata)
+	return c.JSON(err)
 }
