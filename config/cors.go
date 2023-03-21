fc3279985bb13a1bf477e335b6e00e579640e868
rofinafiin <rofinafiisr@gmail.com>
2023-03-21 17:37:27 +0700

    add new

--- config/cors.go
+++ config/cors.go
@@ -20,7 +20,7 @@ var origins = []string{
 	"https://whatsauth.github.io",
 	"https://rofinafiin.github.io",
 	"https://gocroot.github.io/",
-	"https://gocroot-baru.herokuapp.com",
+	"https://gocroot-baru.herokuapp.com/",
 }
 
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
