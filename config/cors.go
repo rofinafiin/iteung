488928cccab66c7a8f87c70a7be4fd01a841c7b4
rofinafiin <rofinafiisr@gmail.com>
2023-03-27 13:47:29 +0700

    allow

--- config/cors.go
+++ config/cors.go
@@ -28,7 +28,7 @@ var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
 var Cors = cors.Config{
 	AllowOrigins:     strings.Join(origins[:], ","),
 	AllowMethods:     "GET,HEAD,OPTIONS,POST,PUT",
-	AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+	AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, token",
 	ExposeHeaders:    "Content-Length",
 	AllowCredentials: true,
 }
